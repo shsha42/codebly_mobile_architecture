891f4a8db71a0fcfe5b03ed3d1f475d47c59e450
YM Kim <m0m32929@naver.com>
2024-07-24 16:29:01 +0900

    xd 추출 첫시도

---
+++ app/src/main/java/com/example/xd_attempt1/________activity.java
@@ -0,0 +1,91 @@
+package com.example.xd_attempt1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ________activity extends Activity {
+
+
+    private View _bg__________;
+    private ImageView ___5;
+    private View _____13;
+    private View _____20;
+    private TextView ______;
+    private View _____9;
+    private TextView _______;
+    private TextView ______________;
+    private View _____16;
+    private TextView _________;
+    private TextView id_example_com;
+    private View _____17;
+    private TextView ________ek1;
+    private TextView _____;
+    private View _____18;
+    private TextView __________;
+    private TextView ________;
+    private ImageView ___6;
+    private ImageView ___9;
+    private TextView _______ek1;
+    private TextView __________ek1;
+    private View _____18_ek1;
+    private TextView _____________;
+    private View _____19;
+    private TextView _____________________;
+    private View _____19_ek1;
+    private ImageView _____13_ek1;
+    private ImageView _____14;
+    private ImageView ___2;
+    private ImageView ___3;
+    private ImageView ___4;
+    private TextView ______ek1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout._______);
+
+
+        _bg__________ = (View) findViewById(R.id._bg__________);
+        ___5 = (ImageView) findViewById(R.id.___5);
+        _____13 = (View) findViewById(R.id._____13);
+        _____20 = (View) findViewById(R.id._____20);
+        ______ = (TextView) findViewById(R.id.______);
+        _____9 = (View) findViewById(R.id._____9);
+        _______ = (TextView) findViewById(R.id._______);
+        ______________ = (TextView) findViewById(R.id.______________);
+        _____16 = (View) findViewById(R.id._____16);
+        _________ = (TextView) findViewById(R.id._________);
+        id_example_com = (TextView) findViewById(R.id.id_example_com);
+        _____17 = (View) findViewById(R.id._____17);
+        ________ek1 = (TextView) findViewById(R.id.________ek1);
+        _____ = (TextView) findViewById(R.id._____);
+        _____18 = (View) findViewById(R.id._____18);
+        __________ = (TextView) findViewById(R.id.__________);
+        ________ = (TextView) findViewById(R.id.________);
+        ___6 = (ImageView) findViewById(R.id.___6);
+        ___9 = (ImageView) findViewById(R.id.___9);
+        _______ek1 = (TextView) findViewById(R.id._______ek1);
+        __________ek1 = (TextView) findViewById(R.id.__________ek1);
+        _____18_ek1 = (View) findViewById(R.id._____18_ek1);
+        _____________ = (TextView) findViewById(R.id._____________);
+        _____19 = (View) findViewById(R.id._____19);
+        _____________________ = (TextView) findViewById(R.id._____________________);
+        _____19_ek1 = (View) findViewById(R.id._____19_ek1);
+        _____13_ek1 = (ImageView) findViewById(R.id._____13_ek1);
+        _____14 = (ImageView) findViewById(R.id._____14);
+        ___2 = (ImageView) findViewById(R.id.___2);
+        ___3 = (ImageView) findViewById(R.id.___3);
+        ___4 = (ImageView) findViewById(R.id.___4);
+        ______ek1 = (TextView) findViewById(R.id.______ek1);
+
+
+        //custom code goes here
+
+    }
+}
